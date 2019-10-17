851d69c736eb822984401cb119d94537b161651e
Anmol Sinha <54975830+stonecoldanmol@users.noreply.github.com>
2019-10-17 14:50:24 +0530

    Add files via upload

---
+++ GameOverActivity.java
@@ -0,0 +1,38 @@
+package com.example.theflyingfish;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    private Button startGameAgain;
+    private TextView DisplayScore;
+    private String score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+
+        score=getIntent().getExtras().get("score").toString();
+
+        startGameAgain=findViewById(R.id.play_again_btn);
+        DisplayScore=findViewById(R.id.displayScore);
+
+        startGameAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent=new Intent(GameOverActivity.this,MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+
+        DisplayScore.setText(score);
+    }
+}
